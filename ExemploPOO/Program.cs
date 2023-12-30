c5d4df06360a20d28c3c4fd5fd9d8a4ba77afc9c
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2023-12-30 00:11:25 -0300

    POO - Abstração e Encapsulamento

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExemploPOO.Models;
+
+//pessoa
+Pessoa p1 = new Pessoa();
+p1.Nome = "Beatriz";
+p1.Idade = 22;
+
+p1.Apresentar();
+
+//contacorrente
+
+ContaCorrente c1 = new ContaCorrente(123,1000);
+ c1.ExibirSaldo();
+ c1.Sacar(500); 
+ c1.ExibirSaldo();
